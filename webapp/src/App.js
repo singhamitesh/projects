5ffc76eef6fce692eac234e63790eec437ca7f2a
singhamitesh <amiteshsingh77974@gmail.com>
2024-07-04 14:51:03 +0530

    amitesh

---
+++ webapp/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Amitesh Singh first Docker project app.</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Surf Website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
